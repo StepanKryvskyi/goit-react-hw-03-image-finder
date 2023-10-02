36d2dfa2074ba47b4af5f3353fb2a7210083816b
StepanKryvskyi <126700161+StepanKryvskyi@users.noreply.github.com>
2023-10-03 01:36:20 +0300

    hw-03 img-search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Container } from './App.styled';
+
+
+export class App extends Component {
+  state = {
+    value: '',
+  };
+  onSubmit = (value) => {
+    this.setState({ value: value });
+  };
+  render() {
+    return (
+      <Container>
+        <Searchbar onSubmit={this.onSubmit} />
+        <ImageGallery value={this.state.value} />
+      </Container>
+    );
+  }
+}
